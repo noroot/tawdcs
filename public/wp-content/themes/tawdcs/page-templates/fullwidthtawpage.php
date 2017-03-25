d1706bae7f753288c6ad05a3bc59b78c36370d61
overmind <seogarry@gmail.com>
2017-03-26 00:46:43 +0300

    Fix banner link

--- public/wp-content/themes/tawdcs/page-templates/fullwidthtawpage.php
+++ public/wp-content/themes/tawdcs/page-templates/fullwidthtawpage.php
@@ -21,7 +21,7 @@ get_header(); ?>
     <div class="container taw-container">
 	<div class="row">
 	    <div class="col-md-12">
-		<a href="http://ko.tawdcs.net" target="_blank"><img src="/wp-content/themes/tawdcs/images/KO_banner.jpg" /></a>
+		<a href="http://ko.tawdcs.org" target="_blank"><img src="/wp-content/themes/tawdcs/images/KO_banner.jpg" /></a>
 	    </div>
 	</div>
     </div>
